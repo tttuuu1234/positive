0b7fc43bb8f20aa762fe736dc63e5e50ad12d199
tttuuu1234 <yidongyi8@gmail.com>
2020-08-26 13:56:21 +0900

    初期ページ作成

---
+++ src/app/Models/PositiveMessage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PositiveMessage extends Model
+{
+    protected $fillable = ['positive_message'];
+}
---
+++ src/resources/views/common/layouts.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <!-- Styles -->
+    <!-- JavaScript -->
+</head>
+<body>
+    <div id="app" class="app">
+        <div class="mt-5">
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
